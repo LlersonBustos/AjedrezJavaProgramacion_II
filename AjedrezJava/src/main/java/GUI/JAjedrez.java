e39e0795f8f576134a6992e42b4d8355dde66778
LlersonBustos <58040827+LlersonBustos@users.noreply.github.com>
2019-12-11 13:20:48 -0500

    Actualizaciones de la GUI
    Fixed:
    -Piezas
    -Color del tablero

---
+++ AjedrezJava/src/main/java/GUI/JAjedrez.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import Tablero.Tablero;
+
+/**
+ *
+ * @author Usuario
+ */
+
+public class JAjedrez {
+    
+     public static void main( String[] args){
+         Tablero tablero = Tablero.crearTableroEstándar();
+         Ventana1 Ventana = new Ventana1();
+     }
+    
+}
+
